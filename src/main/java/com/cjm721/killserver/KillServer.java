919f581b07055a7d9e15ce0d0b2a7e747896bbab
CJ Miller <cjmiller721@gmail.com>
2019-07-04 18:04:33 -0700

    Now Attempts Save, Print Non-Daemon Threads, Kill

--- src/main/java/com/cjm721/killserver/KillServer.java
+++ src/main/java/com/cjm721/killserver/KillServer.java
@@ -1,11 +1,19 @@
 package com.cjm721.killserver;
 
+import net.minecraft.server.MinecraftServer;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.stream.Collectors;
 
 @Mod("killserver")
 public class KillServer {
@@ -20,26 +28,56 @@ public class KillServer {
   @SubscribeEvent
   public void onServerStarting(FMLServerStartingEvent event) {
     Thread serverThread = Thread.currentThread();
-    Thread watcherThread = new Thread(() -> {
-      LOGGER.info("Starting Server Thread Watcher");
-      while(true) {
-        if (!serverThread.isAlive()) {
-          LOGGER.fatal("Server Thread has been detected as dead. Waiting 10 seconds to kill server");
-          try {
-            Thread.sleep(10 * 1000L);
-          } catch (InterruptedException ignored) {
-          }
-          LOGGER.fatal("10 seconds has pasted since Server Thread was dead. KILLING SERVER");
-          System.exit(2);
-        }
-        try {
-          Thread.sleep(10 * 1000L);
-        } catch (InterruptedException ignored) {
-        }
-      }
-    });
+    Thread watcherThread = new Thread(() -> watchServer(event.getServer(), serverThread));
     watcherThread.setDaemon(true);
     watcherThread.setName("KillServer");
     watcherThread.start();
+  }
+
+  private void watchServer(MinecraftServer server, Thread serverThread) {
+    LOGGER.info("Starting Server Thread Watcher");
+    while (true) {
+      if (!serverThread.isAlive()) {
+        LOGGER.fatal("Server Thread has been detected as dead. Attempting Save.");
+        try {
+          Executors.newCachedThreadPool().submit(() -> {
+            try {
+              server.save(false, true, true);
+            } catch (Exception e) {
+              LOGGER.fatal("Exception thrown while saving.", e);
+              e.printStackTrace();
+            }
+          }).get(10, TimeUnit.SECONDS);
+        } catch (InterruptedException ignored) {
+        } catch (ExecutionException e) {
+          LOGGER.fatal("Uncaught Exception while saving. ", e);
+        } catch (TimeoutException e) {
+          LOGGER.fatal("Unable to Save within 10 seconds. Continuing.");
+        }
+        LOGGER.fatal("Waiting 10 seconds before killing in case of non-sync saving triggered.");
+        sleepWait();
+        LOGGER.fatal("10 seconds are up. Printing Non-Daemon Threads: ");
+        for (Map.Entry<Thread, StackTraceElement[]> entry :
+            Thread.getAllStackTraces().entrySet().stream()
+                .filter(e -> !e.getKey().isDaemon())
+                .collect(Collectors.toList())) {
+          LOGGER.fatal(entry.getKey().toString());
+          for (StackTraceElement s : entry.getValue()) {
+            LOGGER.fatal("\tat " + s.getClassName() + "." + s.getMethodName()
+                + "(" + s.getFileName() + ":" + s.getLineNumber() + ")");
+          }
+          LOGGER.fatal("----------------------------------");
+        }
+        LOGGER.fatal("KILLING NOW");
+        System.exit(2);
+      }
+      sleepWait();
+    }
+  }
+
+  private void sleepWait() {
+    try {
+      Thread.sleep(10 *1000);
+    } catch (InterruptedException e) {}
   }
 }
